52fb300aa7208e89f623bf5f3f147c6cea48e3fa
skywolf46 <skywolf461@gmail.com>
2022-10-02 08:57:42 +0900

    feat: 메인 클래스 추가

---
+++ src/main/kotlin/skywolf46/jabytecode/JabyteCode.kt
@@ -0,0 +1,11 @@
+package skywolf46.jabytecode
+
+import skywolf46.jabytecode.data.JabyteConstantInfoBase
+import skywolf46.jabytecode.data.JabyteConstantPoolInfo
+import java.io.File
+
+object JabyteCode {
+    fun init() {
+        JabyteConstantInfoBase.init()
+    }
+}
